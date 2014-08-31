9f1b04b4feeb6df27945dd8940734886816b8acc
Jack Peterson <jack@tinybike.net>
2014-08-31 07:59:40 -0700

    epoch time

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -122,7 +122,7 @@ public:
         //     CTxIn(COutPoint(000000, -1), coinbase 04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73)
         //     CTxOut(nValue=50.00000000, scriptPubKey=0x5F1DF16B2B704C8A578D0B)
         //   vMerkleTree: 4a5e1e
-        const char* pszTimestamp = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
+        const char* pszTimestamp = "Celebration in Liberia slum as Ebola quarantine lifted";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -133,7 +133,7 @@ public:
         genesis.hashPrevBlock = 0;
         genesis.hashMerkleRoot = genesis.BuildMerkleTree();
         genesis.nVersion = 1;
-        genesis.nTime    = 1231006505;
+        genesis.nTime    = 1409497097;
         genesis.nBits    = 0x1d00ffff;
         genesis.nNonce   = 2083236893;
 
@@ -201,7 +201,8 @@ public:
         strDataDir = "testnet3";
 
         // Modify the testnet genesis block so the timestamp is valid for a later start.
-        genesis.nTime = 1296688602;
+        // genesis.nTime = 1296688602;
+        genesis.nTime = 1409496971;
         genesis.nNonce = 414098458;
         hashGenesisBlock = genesis.GetHash();
         assert(hashGenesisBlock == uint256("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
