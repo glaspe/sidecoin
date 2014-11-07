3df13bf406fcea415c96b191e2cc7acd68da1e80
joeykrug <joeykrug@gmail.com>
2014-11-07 13:35:38 -0800

    fixed base58 tests

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -69,8 +69,8 @@ CMainParams::CMainParams()
 
     vSeeds.push_back(CDNSSeedData("crypto.cab", "69.164.196.239"));
 
-    // base58Prefixes[PUBKEY_ADDRESS] = list_of(63); // S prefix
-    base58Prefixes[PUBKEY_ADDRESS] = list_of(0);
+    base58Prefixes[PUBKEY_ADDRESS] = list_of(63); // S prefix
+   // base58Prefixes[PUBKEY_ADDRESS] = list_of(0);
     base58Prefixes[SCRIPT_ADDRESS] = list_of(5);
     base58Prefixes[SECRET_KEY]     = list_of(128);
     base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E);
